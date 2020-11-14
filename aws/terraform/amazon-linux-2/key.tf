c3e0883be8e164ed3b239269dd6434d67af72467
Daniel Dias <danielricardodias@gmail.com>
2020-11-14 22:01:47 +0100

    feat(aws): aWS amazon-linux-2 remote development server deployed with terraform
    Creates a ssh key, ec2 instance and security group using terraform

---
+++ aws/terraform/amazon-linux-2/key.tf
@@ -0,0 +1,34 @@
+resource "random_string" "id" {
+  length = 5
+  special = false
+}
+
+resource "tls_private_key" "generated_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "remote_dev_${random_string.id.result}"
+  public_key = tls_private_key.generated_key.public_key_openssh
+}
+
+output "generated_key_name" {
+  description = "Generated key name"
+  value       = aws_key_pair.generated_key.key_name
+}
+
+output "private_key_pem" {
+  description = "SSH private pem key"
+  value       = tls_private_key.generated_key.private_key_pem
+}
+
+output "public_key_pem" {
+  description = "SSH public pem key"
+  value       = tls_private_key.generated_key.public_key_pem
+}
+
+output "public_key_openssh" {
+  description = "Open SSH public key"
+  value       = tls_private_key.generated_key.public_key_openssh
+}
---
+++ aws/terraform/amazon-linux-2/main.tf
@@ -0,0 +1,21 @@
+module "ec2_remote_development" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version                = "~> 2.0"
+  name                   = "remote_dev_${random_string.id.result}"
+  ami                    = data.aws_ami.amazon-linux-2.id
+  instance_type          = "t3a.medium"
+  key_name               = aws_key_pair.generated_key.key_name
+  vpc_security_group_ids = [aws_security_group.ssh.id]
+
+  subnet_id               = "subnet-055a2e2a1f2c326ba"
+}
+
+output "public-ip" {
+  description = "EC2 public IP"
+  value       = module.ec2_remote_development.public_ip
+}
+
+output "private-ip" {
+  description = "EC2 private IP"
+  value       = module.ec2_remote_development.private_ip
+}
